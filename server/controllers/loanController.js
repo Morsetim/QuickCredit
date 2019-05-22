5988b69e4562ad468c9dcc4755e19edf3fc004ac
Morsetim <mauricium.maurice@yahoo.com>
2019-05-22 11:39:38 +0100

    Ch(Migrate to PostgresDB)
    [Starts #166148080]

--- server/controllers/loanController.js
+++ server/controllers/loanController.js
@@ -6,15 +6,14 @@ env.config();
 
 class LoanController{
   apply(req, res){
-    const {firstname, lastname, email, tenor, amount} = req.body;
+    const {firstName, lastName, email, tenor, amount} = req.body;
     const interest = amount * (5/100);
     const monthlyInstallment =+ amount + interest;
     const paymentInstallment = monthlyInstallment / tenor;
     const balance = amount - paymentInstallment
     const status = 'Pending';
     const sql = 'INSERT INTO loans(firstname, lastname, email, tenor, amount) VALUES($1, $2, $3, $4, $5) RETURNING *';
-    const params = [firstname, lastname, email, tenor, amount];
-
+    const params = [firstName, lastName, email, tenor, amount];
     loanData.query(sql, params).then(loan=>{
       return res.status(201)
                 .json({
@@ -25,6 +24,84 @@ class LoanController{
                         }
                     ]
                 });
-    })
+    }).catch(err =>res.status(500).json({status: 'Failed', message:err.message}))
   }
+  allLoans(req, res){
+    const sql = `SELECT * FROM loans`
+    loansData.query(sql).then(loan=>{
+      return res.status(201)
+                .json({
+                    status : 201,
+                    data : [
+                        {
+                            allLoan:loan
+                        }
+                    ]
+                });
+    }).catch(err =>res.status(500).json({status: 'Failed', message:err.message}))
+  }
+  getOneLoan(req, res){
+    const loanId = req.params;
+    const sql = `SELECT * FROM loans WHERE id=${loanId}`
+    loansData.query(sql).then(loan =>{
+      if(loan){
+      return res.status(201)
+                .json({
+                    status : 201,
+                    data : [
+                        {
+                            specificLoan:loan
+                        }
+                    ]
+                });
+      }else{
+        return res.status(400)
+                .json({
+                    status : 400,
+                    message : `loan with id ${loan} does not exist in your catalogue`
+                });
+      }
+    }).catch(err =>res.status(500).json({status: 'Failed', message:err.message}))
+  }
+  unrepaidLoan(req, res){
+    const sql = `SELECT FROM loans WHERE status=$1 repaid=$2`
+    const params = ['approved', 'false'];
+    loansData.query(sql,params).then(unrepaid=>{
+      if(unrepaid){
+        return res.status(201)
+                .json({
+                    status : 201,
+                    message: 'All unrepaid loans',
+                    unrepaid: unrepaid
+                });
+      }
+      return res.status(400)
+                .json({
+                    status : 400,
+                    message: 'No unrepaid loans'
+                });
+    }).catch(err =>res.status(500).json({status: 'Failed', message:err.message}))
+  }
+  repaidLoan(req, res){
+    const sql = `SELECT FROM loans WHERE status=$1 repaid=$2 balance=$3`
+    const params = ['approved', 'true', 0];
+    loansData.query(sql,params).then(repaid=>{
+      if(repaid){
+        return res.status(201)
+                .json({
+                    status : 201,
+                    message: 'All repaid loans',
+                    repaid: repaid
+                });
+      }
+      return res.status(400)
+                .json({
+                    status : 400,
+                    message: 'No repaid loans'
+                });
+    }).catch(err =>res.status(500).json({status: 'Failed', message:err.message}))
+  }
+
 }
+
+export default new LoanController();
