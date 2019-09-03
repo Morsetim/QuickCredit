3c3303067e994a657da9b00f11488b770f0aabb6
Maurice <mauricium.maurice@yahoo.com>
2019-09-03 09:50:49 +0100

    Final-touch

--- server/middlewares/checkAdmin.js
+++ server/middlewares/checkAdmin.js
@@ -1,7 +1,6 @@
 const checkAdmin = (req, res, next) => {
   
     (req.decoded.isAdmin) ? next() : res.status(200).send({ Message: 'You are not permitted' });
-    console.log(req.decoded)
   }
   
   export default checkAdmin;
